b6718a3f38eaff0a2dd0b18adccd50bebaf304eb
Jack Brandt <jdbrandt@stu.naperville203.org>
2016-04-24 15:05:01 -0500

    Added DCE

--- DoubleColorlessEnergy.java
+++ DoubleColorlessEnergy.java
@@ -2,5 +2,17 @@
 
 public class DoubleColorlessEnergy extends SpecialEnergy
 {
+    public DoubleColorlessEnergy()
+    {
+        super();
+        super.type.add(Type.C);
+        super.type.add(Type.C);
+    }
     
+    public Effect getEffect()
+    {
+        return new NullEffect();
+    }
+    
+}
     
